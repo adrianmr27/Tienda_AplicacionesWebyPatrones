acba8a7102efa6cce34fb874fcda38c87bfd03df
adrian <adria@Adrin>
2024-06-18 21:15:59 -0600

    Finalización Semana 05

---
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -0,0 +1,32 @@
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data // Crea automaticamente los get y set 
+@Entity
+@Table(name="categoria")
+public class Categoria implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private long idCategoria;
+    private String descripcion;
+    private String rutaImagen; // Hibernate lo transforma en ruta_imagen
+    private boolean activo;
+
+    public Categoria() {
+    }
+
+    public Categoria(String descripcion, String rutaImagen, boolean activo) {
+        this.descripcion = descripcion;
+        this.rutaImagen = rutaImagen;
+        this.activo = activo;
+    }
+    
+    
+}
